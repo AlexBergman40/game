4f59dea6d49bfc4d5664e96348e17f55c285496d
Justus Jones <justus1274@gmail.com>
2024-09-10 15:04:41 -0700

    dll files placed correctly, Makefile and generatePossibleMoves tweaked

--- src/generatePossibleMoves.cpp
+++ src/generatePossibleMoves.cpp
@@ -7,7 +7,7 @@
 generatePossibleMoves::generatePossibleMoves() {}
 
 // Initialize with currentPiece, pieceType, and moveHistory
-generatePossibleMoves::generatePossibleMoves(const uint64_t &square, const int &pieceType, const std::vector<std::vector<uint64_t>> &moveHistory, const uint64_t &piecesOnBoard, const uint64_t &friendlyPieces, const uint64_t &enemyPieces, std::vector<uint64_t*> pieceTypeContainer)
+generatePossibleMoves::generatePossibleMoves(const uint64_t &square, const int &pieceType, const std::vector<std::vector<uint64_t>> &moveHistory, const uint64_t &piecesOnBoard, const uint64_t &friendlyPieces, const uint64_t &enemyPieces, std::vector<uint64_t *> pieceTypeContainer)
 {
   std::cout << "Generating possible moves for " << pieceType << " at " << std::bitset<64>(square) << std::endl;
   // setting square
@@ -17,6 +17,19 @@ generatePossibleMoves::generatePossibleMoves(const uint64_t &square, const int &
   this->enemyPieces = enemyPieces;
   this->pieceTypeContainer = pieceTypeContainer;
   this->moveHistory = moveHistory;
+
+  // finding same rank
+  uint64_t rank = 0b1111111100000000000000000000000000000000000000000000000000000000;
+  for (int i = 0; i < 8; i++)
+  {
+    if (square & rank)
+    {
+      sameRank = rank;
+      break;
+    }
+    rank = rank >> 8;
+  }
+
   // calling correct member function
   (this->*funcs[pieceType])();
 }
@@ -30,31 +43,31 @@ void generatePossibleMoves::recursiveMove(uint64_t recursivePosition, int north,
   // limit west/east movement, north/south movement
   if (north == 0 && west == 0) // can't move without any direction
     return;
-  
+
   uint64_t squareNext = recursivePosition;
-  
+
   switch (north)
   {
-    case 1:
-      squareNext = squareNext >> 8;
-      break;
-    case -1:
-      squareNext = squareNext << 8;
-      break;
-    default:
-      break;
+  case 1:
+    squareNext = squareNext >> 8;
+    break;
+  case -1:
+    squareNext = squareNext << 8;
+    break;
+  default:
+    break;
   }
 
   switch (west)
   {
-    case 1:
-      squareNext = squareNext >> 1;
-      break;
-    case -1:
-      squareNext = squareNext << 1;
-      break;
-    default:
-      break;
+  case 1:
+    squareNext = squareNext >> 1;
+    break;
+  case -1:
+    squareNext = squareNext << 1;
+    break;
+  default:
+    break;
   }
   // rook can move up indefinitely
   if (squareNext == 0 /*top and bottom of board*/)
@@ -123,22 +136,14 @@ void generatePossibleMoves::generateForWhitePawn()
     uint64_t left = square >> 1;
 
     // check right for pawn => check for previous 2 square move
-    std::cout << "           Black Pawns thisrn: " << std::bitset<64>(~moveHistory[moveHistory.size()-2][BLACKPAWN]) << std::endl;
-    if (right & ~leftMostColumn 
-      && right & ~moveHistory[moveHistory.size()-2][BLACKPAWN]
-      && (right >> 16) & ~moveHistory.back()[BLACKPAWN]
-      && (right >> 16) & moveHistory[moveHistory.size()-2][BLACKPAWN]
-      && right & moveHistory.back()[BLACKPAWN])
+    std::cout << "           Black Pawns thisrn: " << std::bitset<64>(~moveHistory[moveHistory.size() - 2][BLACKPAWN]) << std::endl;
+    if (right & ~leftMostColumn && right & ~moveHistory[moveHistory.size() - 2][BLACKPAWN] && (right >> 16) & ~moveHistory.back()[BLACKPAWN] && (right >> 16) & moveHistory[moveHistory.size() - 2][BLACKPAWN] && right & moveHistory.back()[BLACKPAWN])
     {
       std::cout << "EN PASSANT" << std::endl;
       possibleMoves |= (right >> 8);
     }
     // check left => check for previous 2 square move
-    if (left & ~rightMostColumn 
-      && left & ~moveHistory[moveHistory.size()-2][BLACKPAWN]
-      && (left >> 16) & ~moveHistory.back()[BLACKPAWN]
-      && (left >> 16) & moveHistory[moveHistory.size()-2][BLACKPAWN]
-      && left & moveHistory.back()[BLACKPAWN])
+    if (left & ~rightMostColumn && left & ~moveHistory[moveHistory.size() - 2][BLACKPAWN] && (left >> 16) & ~moveHistory.back()[BLACKPAWN] && (left >> 16) & moveHistory[moveHistory.size() - 2][BLACKPAWN] && left & moveHistory.back()[BLACKPAWN])
     {
       std::cout << "EN PASSANT" << std::endl;
       possibleMoves |= (left >> 8);
@@ -146,7 +151,7 @@ void generatePossibleMoves::generateForWhitePawn()
   }
 }
 
-void generatePossibleMoves::generateForWhiteRook() 
+void generatePossibleMoves::generateForWhiteRook()
 {
   std::cout << "generating for a white rook" << std::endl;
   recursiveMove(square, 1, 0);
@@ -155,11 +160,40 @@ void generatePossibleMoves::generateForWhiteRook()
   recursiveMove(square, 0, -1);
 }
 
-void generatePossibleMoves::generateForWhiteKnight() 
+void generatePossibleMoves::generateForWhiteKnight()
 {
-  std::cout << "generating for a white knight" << std::endl;
+  std::cout << "generating for a knight" << std::endl;
+
+  std::vector<uint64_t> knightMoves = {
+      (square >> 6),
+      (square >> 15),
+      (square << 10),
+      (square << 17),
+      (square << 6),
+      (square << 15),
+      (square >> 10),
+      (square >> 17)};
+
+  for (int i = 0; i < knightMoves.size(); i++)
+  {
+    int64_t move = knightMoves[i];
+    int64_t column, secondMostColumn;
+    column = (i > 4) ? rightMostColumn, secondMostColumn = secondRightMostColumn : leftMostColumn, secondMostColumn = secondLeftMostColumn;
+
+    // checking moves for off the board and friendly pieces
+    if (move & ~sameRank)
+    {
+      if (i % 2 == 0) // even i => move is out 2 squares
+        if (move & secondMostColumn)
+          continue;
+        else if (move & column || move & friendlyPieces) // if move is off the board or is friendly)
+          continue;
+        else
+          possibleMoves |= move;
+    }
+  }
 }
-void generatePossibleMoves::generateForWhiteBishop() 
+void generatePossibleMoves::generateForWhiteBishop()
 {
   std::cout << "generating for a white bishop" << std::endl;
   recursiveMove(square, 1, 1);
@@ -167,15 +201,43 @@ void generatePossibleMoves::generateForWhiteBishop()
   recursiveMove(square, -1, 1);
   recursiveMove(square, -1, -1);
 }
-void generatePossibleMoves::generateForWhiteQueen() 
+void generatePossibleMoves::generateForWhiteQueen()
 {
   std::cout << "generating for a white queen" << std::endl;
   generateForWhiteBishop();
   generateForWhiteRook();
 }
-void generatePossibleMoves::generateForWhiteKing() 
+void generatePossibleMoves::generateForWhiteKing()
 {
-  std::cout << "generating for a white king" << std::endl;
+  std::cout << "generating for a king" << std::endl;
+
+  std::vector<uint64_t> kingMovesHorizontal = {
+      (square >> 7),
+      (square << 1),
+      (square << 9),
+      (square >> 9),
+      (square >> 1),
+      (square << 7),
+  };
+  std::vector<uint64_t> kingMovesVertical = {
+      (square >> 8),
+      (square << 8),
+  };
+
+  for (int i = 0; i < kingMovesHorizontal.size(); i++)
+  {
+    uint64_t move = kingMovesHorizontal[i];
+    uint64_t column = (i > 3) ? rightMostColumn : leftMostColumn;
+    if (move & column || move & friendlyPieces) // if move is off the board or is friendly
+      continue;
+    possibleMoves |= move;
+  }
+  for (uint64_t move : kingMovesVertical)
+  {
+    if (move == 0 || move & friendlyPieces) // if move is off the board or is friendly
+      continue;
+    possibleMoves |= move;
+  }
 }
 
 void generatePossibleMoves::generateForBlackPawn()
@@ -212,44 +274,36 @@ void generatePossibleMoves::generateForBlackPawn()
   {
     uint64_t right = square << 1;
     uint64_t left = square >> 1;
-    if (right & ~leftMostColumn 
-      && right & ~moveHistory[moveHistory.size()-2][WHITEPAWN]
-      && (right << 16) & ~moveHistory.back()[WHITEPAWN]
-      && (right << 16) & moveHistory[moveHistory.size()-2][WHITEPAWN]
-      && right & moveHistory.back()[WHITEPAWN])
+    if (right & ~leftMostColumn && right & ~moveHistory[moveHistory.size() - 2][WHITEPAWN] && (right << 16) & ~moveHistory.back()[WHITEPAWN] && (right << 16) & moveHistory[moveHistory.size() - 2][WHITEPAWN] && right & moveHistory.back()[WHITEPAWN])
     {
       std::cout << "EN PASSANT" << std::endl;
       possibleMoves |= (right << 8);
     }
     // check left => check for previous 2 square move
-    if (left & ~rightMostColumn 
-      && left & ~moveHistory[moveHistory.size()-2][WHITEPAWN]
-      && (left << 16) & ~moveHistory.back()[WHITEPAWN]
-      && (left << 16) & moveHistory[moveHistory.size()-2][WHITEPAWN]
-      && left & moveHistory.back()[WHITEPAWN])
+    if (left & ~rightMostColumn && left & ~moveHistory[moveHistory.size() - 2][WHITEPAWN] && (left << 16) & ~moveHistory.back()[WHITEPAWN] && (left << 16) & moveHistory[moveHistory.size() - 2][WHITEPAWN] && left & moveHistory.back()[WHITEPAWN])
     {
       std::cout << "EN PASSANT" << std::endl;
       possibleMoves |= (left << 8);
     }
   }
 }
-void generatePossibleMoves::generateForBlackRook() 
+void generatePossibleMoves::generateForBlackRook()
 {
   generateForWhiteRook();
 }
-void generatePossibleMoves::generateForBlackKnight() 
+void generatePossibleMoves::generateForBlackKnight()
 {
-
+  generateForWhiteKnight();
 }
-void generatePossibleMoves::generateForBlackBishop() 
+void generatePossibleMoves::generateForBlackBishop()
 {
   generateForWhiteBishop();
 }
-void generatePossibleMoves::generateForBlackQueen() 
+void generatePossibleMoves::generateForBlackQueen()
 {
   generateForWhiteQueen();
 }
-void generatePossibleMoves::generateForBlackKing() 
+void generatePossibleMoves::generateForBlackKing()
 {
-
+  generateForWhiteKing();
 }
--- src/generatePossibleMoves.hpp
+++ src/generatePossibleMoves.hpp
@@ -5,26 +5,27 @@
 class generatePossibleMoves
 {
 private:
-
   enum pieceTypes
   {
-      WHITEPAWN,
-      WHITEROOK,
-      WHITEBISHOP,
-      WHITEKNIGHT,
-      WHITEQUEEN,
-      WHITEKING,
-      BLACKPAWN,
-      BLACKROOK,
-      BLACKBISHOP,
-      BLACKKNIGHT,
-      BLACKQUEEN,
-      BLACKKING
+    WHITEPAWN,
+    WHITEROOK,
+    WHITEBISHOP,
+    WHITEKNIGHT,
+    WHITEQUEEN,
+    WHITEKING,
+    BLACKPAWN,
+    BLACKROOK,
+    BLACKBISHOP,
+    BLACKKNIGHT,
+    BLACKQUEEN,
+    BLACKKING
   };
 
   static const uint64_t endOfColumn = 0b1000000110000001100000011000000110000001100000011000000110000001;
-  static const uint64_t leftMostColumn  = 0b0000000100000001000000010000000100000001000000010000000100000001;
+  static const uint64_t leftMostColumn = 0b0000000100000001000000010000000100000001000000010000000100000001;
+  static const uint64_t secondLeftMostColumn = 0b0000001000000010000000100000001000000010000000100000001000000010;
   static const uint64_t rightMostColumn = 0b1000000010000000100000001000000010000000100000001000000010000000;
+  static const uint64_t secondRightMostColumn = 0b0100000001000000010000000100000001000000010000000100000001000000;
 
   uint64_t square = 0;
   std::vector<std::vector<uint64_t>> moveHistory = {};
@@ -32,10 +33,11 @@ private:
   uint64_t friendlyPieces = 0;
   uint64_t enemyPieces = 0;
   uint64_t possibleMoves = 0;
+  uint64_t sameRank = 0;
 
   void recursiveMove(uint64_t square, int north, int west);
 
-  std::vector<uint64_t*> pieceTypeContainer;
+  std::vector<uint64_t *> pieceTypeContainer;
 
   std::vector<void (generatePossibleMoves::*)()> funcs = {
       &generatePossibleMoves::generateForWhitePawn,
@@ -67,13 +69,13 @@ private:
 
 public:
   generatePossibleMoves();
-  generatePossibleMoves(const uint64_t &square, 
-    const int &pieceType,
-    const std::vector<std::vector<uint64_t>> &moveHistory, 
-    const uint64_t &piecesOnBoard, 
-    const uint64_t &friendlyPieces, 
-    const uint64_t &enemyPieces, 
-    std::vector<uint64_t *> pieceTypeContainer);
+  generatePossibleMoves(const uint64_t &square,
+                        const int &pieceType,
+                        const std::vector<std::vector<uint64_t>> &moveHistory,
+                        const uint64_t &piecesOnBoard,
+                        const uint64_t &friendlyPieces,
+                        const uint64_t &enemyPieces,
+                        std::vector<uint64_t *> pieceTypeContainer);
   ~generatePossibleMoves();
 
   uint64_t generate() { return possibleMoves; }
